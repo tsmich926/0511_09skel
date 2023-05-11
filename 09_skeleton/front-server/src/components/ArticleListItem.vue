ad4b8ab1b03ecb66a3a072bd8e8a339b3050caa8
tsmich926@naver.com <tsmich926@naver.com>
2023-05-11 17:40:58 +0900

    sf

---
+++ 09_skeleton/front-server/src/components/ArticleListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h5>{{article.id}}</h5>
+    <p>{{article.title}}</p>
+    <router-link :to="{
+      name:'Detailview', 
+      params:{id:article.id}}">
+      [detail]
+      </router-link>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleListItem',
+  props:{
+    article:Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 09_skeleton/front-server/src/store/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+
+const API_URL = 'http://127.0.0.1:8000'
+
+Vue.use(Vuex)
+
+
+export default new Vuex.Store({
+  state: {
+    articles: [
+      {
+        id: 1,
+        title: '제목',
+        content: '내용'
+      },
+      {
+        id: 2,
+        title: '제목2',
+        content: '내용2'
+      },
+    ],
+  },
+  getters: {
+  },
+  mutations: { 
+    GET_ARTICLES(state,articles){
+      state.articles = articles
+    }
+  },
+  actions: {
+    getArticles(context){
+      axios({
+        method:'get',
+        url: `${API_URL}/api/v1/articles`,
+      })
+      .then((res)=>{
+        // console.log(res,context)
+        context.commit('GET_ARTICLES',res.data)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
+  },
+  modules: {
+  }
+})
